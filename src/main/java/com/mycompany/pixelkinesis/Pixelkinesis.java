7360524d3ee9bc38504385bf48ca2f2edc9b3b5a
marquistallman <daviricanun@gmail.com>
2025-10-20 08:57:59 -0500

    Primer commit desde Cursor

---
+++ src/main/java/com/mycompany/pixelkinesis/Pixelkinesis.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.pixelkinesis;
+
+/**
+ *
+ * @author davir
+ */
+public class Pixelkinesis {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
